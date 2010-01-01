1447f624147c4b5853a62e51ccd3ad6f74aae980
prahaai <prahaai@gmail.com>
2010-01-01 21:18:16 +0000

    Database and file password is now MD5 hexdigest. AddFile now adds date and time for every new version of the file. Started work to GetProperties.

---
+++ test_view_data.py
@@ -0,0 +1,17 @@
+
+import sqlite3
+from Crypto.Hash import MD4
+
+conn = sqlite3.connect('Data.prv')
+c = conn.cursor()
+
+for line in c.execute('select * from prv').fetchmany(5):
+    if not line[0] : file1 = line[1]
+    print( line )
+
+print
+md4 = MD4.new( file1 )
+filename = 't'+md4.hexdigest()
+
+for line in c.execute('select * from %s' % filename).fetchmany(5):
+    print( line )
