ae35955a43d3e38cff9e00fd223d7930f5f2d5bd
prahaai <prahaai@gmail.com>
2010-04-05 09:51:03 +0000

    Minor changes.

--- __build_WIN32__.bat
+++ __build_WIN32__.bat
@@ -1,6 +1,10 @@
 
 SET PATH="C:\Python26\Lib\site-packages\PyQt4\bin;C:\Python31\Lib\site-packages\PyQt4\bin;"+%PATH%
-
 c:\python26\python.exe build_win32.py build_exe
+
+copy PB.ico C:\private-briefcase /B /Y
+
+:: Must also copy "imageformats" folder from C:\PythonXX\Lib\site-packages\PyQt4,
+:: into C:\private-briefcase, or wherever you build your executable.
 
 pause
